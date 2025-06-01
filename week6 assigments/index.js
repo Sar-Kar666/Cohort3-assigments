119e79211d8e2fa43b993b53c524941643e0f7dc
Sar-Kar666 <diptanusarkar699@gmail.com>
2025-06-02 03:11:03 +0530

    made some progress

--- week6 assigments/index.js
+++ week6 assigments/index.js
@@ -12,6 +12,12 @@ function logger(req,res,next){
     next();
 }
 
+app.get("/",function(req,res){
+        res.sendFile(__dirname+"/index.html");
+})
+
+
+
 function generateToken() {
     let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
@@ -23,7 +29,7 @@ function generateToken() {
     return token;
 }
 
-app.post("/singup",logger,function (req,res){
+app.post("/signup",logger,function (req,res){
     const username=req.body.username;
     const password=req.body.password;
 
