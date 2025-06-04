27fa34fd97fad017f4b8fdce379f929c741b42ba
Sar-Kar666 <diptanusarkar699@gmail.com>
2025-06-05 02:42:43 +0530

    added signup and signin functionality

--- week6.2 assigments(todo -app with authintication)/index.js
+++ week6.2 assigments(todo -app with authintication)/index.js
@@ -1,9 +1,59 @@
 const express =require('express');
 
 const app= express();
+app.use(express.json());
 
-app.get("/",function(req,res){
+const users=[];
+
+
+app.get("/login",function(req,res){
+    res.sendFile(__dirname+"/login.html");
+})
+app.get("/todos",function(req,res){
     res.sendFile(__dirname+"/index.html");
 })
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/signup.html");
+})
+
+
+
+app.post("/signup",function(req,res){
+    const username=req.body.username;
+    const password=req.body.password;
+
+    users.push({
+        username: username,
+        password: password
+    })
+
+    res.json({
+        message: "you are signed in"
+    })
+    
+
+});
+
+app.post("/signin",function(req,res){
+    const username=req.body.username;
+    const password=req.body.password;
+
+    let user=null;
+    for(let i=0;i<users.length;i++){
+        if(users[i].username===username && users[i].password===password){
+            user=users[i];
+        }
+        else{
+            
+            res.json({
+                message:"wrong Credentials"
+            })
+        }   
+     }
+    res.json({
+        message:"you are logged in"
+    })
+
+});
 
 app.listen(4000);
