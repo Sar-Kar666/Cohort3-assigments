986426a22e094a59bbd934caaba03f05937e07bb
Sar-Kar666 <diptanusarkar699@gmail.com>
2025-06-12 11:51:21 +0530

    updated

--- week-9-React/src/App.jsx
+++ week-9-React/src/App.jsx
@@ -1,7 +1,40 @@
+import { useState } from "react";
+
+
 function App() {
-
-
-  return 
+  return (<div>
+    
+    <Display/>
+    </div>
+  
+)
 }
+function Display(){
+ const [count,setCount]=useState(0);
+
+
+ function increaseCounter(){
+  setCount(count+1);
+
+  }
+
+
+
+  function decreaseCounter(){
+  setCount(count-1);
+
+  }
+
+
+  return (
+  <div>
+   <h1>{count}</h1>
+  <button onClick={increaseCounter}>Increase count </button>
+  <button onClick={decreaseCounter}>Decrease count </button>
+  </div>
+  )
+}
+
+
 
 export default App
