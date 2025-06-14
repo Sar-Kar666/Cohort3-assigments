4aaa7f9b2b35ee81bfd6b7ce957ca75b72a062bf
Sar-Kar666 <diptanusarkar699@gmail.com>
2025-06-14 10:01:34 +0530

    introduction to custom hooks

---
+++ week-11-custom-hooks/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react"
+
+function useCounter(){
+  const [count,setCount]=useState(0);
+
+  function increaseCount(){
+    setCount(c=>c+1)
+  }
+
+  return{
+    count:count,
+    increaseCount: increaseCount
+  }
+}
+
+function App() {
+  const {count,increaseCount}=useCounter();
+  return <div >
+    {count }<br/>
+    <button onClick={increaseCount}>Increase Count</button>
+  </div>
+}
+
+export default App
