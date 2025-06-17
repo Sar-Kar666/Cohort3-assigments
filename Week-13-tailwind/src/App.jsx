0886053e242d06a6672d34940db8dd9a062beb7f
Sar-Kar666 <diptanusarkar699@gmail.com>
2025-06-17 08:00:26 +0530

    initiail

---
+++ Week-13-tailwind/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <div>
+     <div  >Webinar.gg</div>
+     <div >Verfiy your age</div>
+     <div>Please confirm your birthyear.This data will not be stored </div>
+     <div><input></input></div>
+     <div><button>Continue</button></div>
+    </div>
+  )
+}
+
+export default App
