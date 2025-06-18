e1f1a2cecb01008bd529d13bd05495b5004065c2
Sar-Kar666 <diptanusarkar699@gmail.com>
2025-06-18 15:34:34 +0530

    changed

--- week-14-ts/src/index.ts
+++ week-14-ts/src/index.ts
@@ -1,1 +1,18 @@
-console.log("hiii");
+interface User {
+	name: string;
+	age: number;
+}
+
+function sumOfAge(user1: User, user2: User) {
+  return user1.age + user2.age;
+};
+
+// Example usage
+const result = sumOfAge({
+	name: "harkirat",
+	age: 20
+}, {
+	name: "raman",
+	age: 21
+});
+console.log(result); // Output: 9
